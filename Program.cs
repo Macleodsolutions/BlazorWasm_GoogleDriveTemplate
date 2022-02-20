f1c51a4a263a61623d467aa8af0c33b2efd7eefc
William <wmacleod@cadmakers.com>
2022-02-20 13:33:29 -0800

    Added google drive api setup and usage examples

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm_GoogleDriveTemplate;
 
@@ -6,13 +8,24 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-
+builder.Services.AddScoped<CustomAuthenticationMessageHandler>();
+builder.Services.AddHttpClient("auth", opt => { opt.BaseAddress = new Uri("https://www.googleapis.com"); })
+    .AddHttpMessageHandler<CustomAuthenticationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("auth"));
 builder.Services.AddOidcAuthentication(options =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
+    //options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.appdata");
+    options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.file");
 });
 
 await builder.Build().RunAsync();
+
+public class CustomAuthenticationMessageHandler : AuthorizationMessageHandler
+{
+    public CustomAuthenticationMessageHandler(IAccessTokenProvider provider, NavigationManager navigationManager) :
+        base(provider, navigationManager)
+    {
+        ConfigureHandler(new string[] {"https://www.googleapis.com"});
+    }
+}
